60bf963d7171c7ff54e5498138c2181beb25245d
Rudy <rudy@stashaway.com>
2021-11-05 13:50:12 +0800

    new kata for password verifier

---
+++ exercises/password-verifier/sandbox/password-verifier.test.ts
@@ -0,0 +1,5 @@
+import { password-verifier } from './password-verifier'
+
+it('password-verifier', () => {
+  expect(password-verifier()).toEqual('change me here')
+})
---
+++ exercises/password-verifier/sandbox/password-verifier.ts
@@ -0,0 +1,1 @@
+export const password-verifier = () => 'change me here'
