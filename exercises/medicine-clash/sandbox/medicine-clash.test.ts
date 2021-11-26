ae6dafdc9b517c4b154ab87562fb0669ac768f81
Rudy <rudy@stashaway.com>
2021-11-26 14:42:20 +0800

    add solution and kata for medicine prescription

--- exercises/medicine-clash/sandbox/medicine-clash.test.ts
+++ exercises/medicine-clash/sandbox/medicine-clash.test.ts
@@ -1,5 +1,23 @@
-import { medicine-clash } from './medicine-clash'
+import { Person, dateList } from './Person'
+import { Prescription } from './Prescription'
 
-it('medicine-clash', () => {
-  expect(medicine-clash()).toEqual('change me here')
+it('should return no clash if there is no prescriptions', () => {
+  const prescription = new Prescription('any', new Date(), 1)
+  const person = new Person()
+  person.addPrescription(prescription);
+  expect(person.clash()).toEqual(false);
+})
+
+it('returns list of dates', () => {
+  expect(dateList(new Date(), 3)).toEqual(['2021-11-12', '2021-11-13', '2021-11-14'])
+})
+
+it.skip('should return clash dates', () => {
+  const today = new Date()
+  const ibuprofen = new Prescription('ibuprofen', today, 10)
+  const xanex = new Prescription('xanex', today, 1)
+  const person = new Person()
+  person.addPrescription(ibuprofen);
+  person.addPrescription(xanex);
+  expect(person.clash()).toEqual([{ date: today, medicines: ['ibuprofen', 'xanex']}]);
 })
